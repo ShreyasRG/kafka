e4767afa2dd1c38d5e269403f4c3934181727a84
ShreyasRG <shreyasgowda180@gmail.com>
2022-11-08 23:58:12 +0530

    changes commited

---
+++ kafka/src/main/java/com/kafka/demo/ProducerDemo.java
@@ -0,0 +1,41 @@
+package com.kafka.demo;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+
+        log.info("hello world");
+
+        //create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create producer
+        KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
+
+        //create a producer record
+        ProducerRecord<String,String> producerRecord = new ProducerRecord<>("demo-java","hello Kafka u have been played...");
+
+        //send data
+        producer.send(producerRecord);
+
+        //flush
+        producer.flush();
+
+        //flush and close
+        producer.close();
+    }
+}
